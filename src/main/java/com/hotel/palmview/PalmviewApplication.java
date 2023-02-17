9ea354b7344e2c8cb3bfbc9364f5ed192a7481dd
Bishu Roy <biroy1393@gmail.com>
2023-02-17 14:03:47 +0530

    Initial commit

---
+++ src/main/java/com/hotel/palmview/PalmviewApplication.java
@@ -0,0 +1,13 @@
+package com.hotel.palmview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PalmviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PalmviewApplication.class, args);
+	}
+
+}
